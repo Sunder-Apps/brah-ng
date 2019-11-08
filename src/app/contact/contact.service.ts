5818c578d6593a31d56cc83ee93a89f763d2703f
Samuel Underwood <sam@sunderapps.com>
2019-11-08 12:56:50 -0500

    Removed Rise of Locus

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Contact } from './contact';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
   url:string = 'https://sunder-functions20190319082035.azurewebsites.net/api/SendEmail'
-  
   constructor(private httpClient:HttpClient) { }
 
   send (contact:Contact) {
